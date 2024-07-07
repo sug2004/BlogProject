4952d019350743e8e8d708c5da91ff280acb4e2a
Sugrish V M <vmsugrish2004@gmail.com>
2024-07-08 02:25:36 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React, { PureComponent } from 'react'
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard'
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        app
-      </div>
-    )
-  }
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
+    </Routes>
+    
+    </BrowserRouter>
+  )
 }
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard() {
+  return (
+    <div>
+      DashBoard
+    </div>
+  )
+}
